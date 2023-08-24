99a730423f5e6c9681914472fde33545c634f060
sysatom <sysatom@gmail.com>
2023-08-24 18:29:07 +0800

    opt utils

--- server/extra/utils/file_test.go
+++ server/extra/utils/file_test.go
@@ -1,10 +1,38 @@
 package utils
 
 import (
-	"github.com/stretchr/testify/require"
 	"testing"
 )
 
 func TestFileExist(t *testing.T) {
-	require.True(t, FileExist("./string.go"))
+	type args struct {
+		name string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "file_exist",
+			args: args{
+				name: "./string.go",
+			},
+			want: true,
+		},
+		{
+			name: "file_not_exist",
+			args: args{
+				name: "./not_exist.test",
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := FileExist(tt.args.name); got != tt.want {
+				t.Errorf("FileExist() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
